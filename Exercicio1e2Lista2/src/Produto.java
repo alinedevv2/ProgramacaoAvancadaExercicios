533c7e2ed676453c08f8a5e7910314fff7ff64ac
alinedevv2 <115108373+alinedevv2@users.noreply.github.com>
2022-10-07 00:34:39 -0300

    Add files via upload

---
+++ Exercicio1e2Lista2/src/Produto.java
@@ -0,0 +1,14 @@
+//Exercício 2 - Lista 2
+
+import java.util.Date;
+
+public class Produto {
+    private int id;
+    private String nome;
+    private String descricao;
+    private Date validade;
+
+    public int getCod(){
+        return id;
+    }
+}
